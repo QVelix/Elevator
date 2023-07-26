599f41c23bda0d35cfa45fbb6fda343f776d7e22
Maxim Bogachew <39808785+QVelix@users.noreply.github.com>
2023-07-26 14:00:05 +0400

    етод передвижения лифта

--- Classes/elevator.cs
+++ Classes/elevator.cs
@@ -1,6 +1,20 @@
 namespace Classes{
     public class Elevator{
-        private string status = "";
+        private string elevatorStatus = "";
         private int position = 1;
+        private bool doorClose = true;
+
+        void callUp(int floor){
+            if(this.position == floor){
+                Console.WriteLine("Лифт уже на этаже");
+            }else if(floor>20||floor<1){
+                Console.WriteLine("Ошибка, нельзя вызвать лифт на несуществующий этаж");
+            }else{
+                Console.WriteLine("Лифт едет, подождите");
+                Thread.Sleep(1000*Math.Abs(position-floor));
+                this.position = floor;
+                Console.WriteLine("Лифт приехал");
+            }
+        }
     }
 }
