04d4f45c58fabef60190e218b95c27a690d188a5
Maxim Bogachew <39808785+QVelix@users.noreply.github.com>
2023-07-27 14:52:22 +0400

    Попытка вызвать лифт

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Classes;
+
+
+Elevator firstElevator = new Elevator();
+Elevator secondElevator = new Elevator();
+Floor floor = new Floor(firstElevator, secondElevator);
+
+class Person{
+    public int position = 1;
+    private Floor floor;
+
+    public Person(Floor floor){
+        this.floor = floor;
+    }
+    void callElevator(){
+        Console.WriteLine("Подходит к кнопке");
+        Console.WriteLine("Нажимает кнопку");
+        Random rnd = new Random();
+        floor.callUp(position,rnd.Next(1,2));
+        //while()
+    }
+}
